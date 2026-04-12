8880e35c2780b6e23b962879a4be5df3db4dee6e
Deepanshu Singh <deepanshusingh142@gmail.com>
2026-04-12 17:36:56 +0530

    mager sort

--- marge_sort.py
+++
@@ -1,4 +0,0 @@
-arr = [20,10,9,1,6,8,3]
-mid = len(arr) //2
-left = arr[:mid]
-right = arr[mid:]
---
+++ merge_sort.py
@@ -0,0 +1,9 @@
+arr = [20,10,9,1,6,8,3]
+#ending part length of array
+start_index= 0
+end_index = len(arr)-1
+def merge(arr, start_index, end_index):
+    if start_index >= end_index:
+
+    
+   
